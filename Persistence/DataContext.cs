612a6b33cf9fb24bdb8e00193cb9aec61a861e6c
Dell <dell@DESKTOP-NLH0UUR>
2020-10-05 10:17:57 +0200

    End of section 4

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 using Domain;
 
 namespace Persistence
@@ -12,6 +10,7 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
